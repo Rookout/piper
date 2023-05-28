b96d29711b5bdaba37317962c542b10fe492e889
Gosha <gosha@rookout.com>
2023-05-28 06:33:33 +0300

    basic gin server

---
+++ cmd/piper/piper.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	server "github.com/rookout/piper/pkg/server"
+)
+
+func main() {
+	server.Start()
+}
---
+++ pkg/server/server.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Start() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
