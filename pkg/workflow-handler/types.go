3d9776a31f7c32fa284a342d92e5043497296752
Gosha <gosha@rookout.com>
2023-06-06 15:59:32 +0300

    webhook handler interface

--- pkg/workflow-handler/types.go
+++ pkg/workflow-handler/types.go
@@ -1,8 +1,9 @@
 package workflow_handler
 
 import {
-	workflow "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	v1alpha1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
 }
+
 type WorkflowConfig struct {
 }
 
