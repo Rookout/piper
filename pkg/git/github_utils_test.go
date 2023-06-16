eb5de701e895dfa51a77ada42184325479c93198
qadiludmer <124146331+qadiludmer@users.noreply.github.com>
2023-06-16 19:30:14 +0300

    Fix: Improve UT to IsOrgWebhookEnabled (#7)
    * add uts
    
    * nit

--- pkg/git/github_utils_test.go
+++ pkg/git/github_utils_test.go
@@ -2,32 +2,59 @@ package git
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/google/go-github/v52/github"
 	"github.com/rookout/piper/pkg/conf"
+	assertion "github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
 )
 
 func TestIsOrgWebhookEnabled(t *testing.T) {
+	//
+	// Prepare
+	//
 	client, mux, _, teardown := setup()
 	defer teardown()
+
+	active := true
+	hookName := "web"
+	config := make(map[string]interface{})
+	config["url"] = "https://bla.com"
+	Hooks := github.Hook{
+		Active: &active,
+		Name:   &hookName,
+		Config: config,
+	}
+	jsonBytes, _ := json.Marshal(&[]github.Hook{Hooks})
 
 	mux.HandleFunc("/orgs/test/hooks", func(w http.ResponseWriter, r *http.Request) {
 		testMethod(t, r, "GET")
 		testFormValues(t, r, values{})
-		fmt.Fprint(w, `[{"id":1}, {"id":2}]`)
+		_, _ = fmt.Fprint(w, string(jsonBytes))
 	})
 
 	c := GithubClientImpl{
 		client: client,
 		cfg: &conf.Config{
 			GitConfig: conf.GitConfig{
-				OrgName: "test",
+				OrgName:    "test",
+				WebhookURL: "https://bla.com",
 			},
 		},
 	}
-
 	ctx := context.Background()
-	isOrgWebhookEnabled(ctx, &c)
 
+	//
+	// Execute
+	//
+	hooks, isEnabled := isOrgWebhookEnabled(ctx, &c)
+
+	//
+	// Assert
+	//
+	assert := assertion.New(t)
+	assert.True(isEnabled)
+	assert.NotNil(t, hooks)
 }
