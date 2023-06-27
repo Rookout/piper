3a8ed9c2abf2a2f711da6642f1617ef89d1d3119
GoshaDozoretz <106976988+gosharo@users.noreply.github.com>
2023-06-27 10:39:58 +0300

    fix: remove argo configuration requierments (#55)
    Signed-off-by: Gosha <gosha@rookout.com>

--- pkg/conf/workflow_server.go
+++ pkg/conf/workflow_server.go
@@ -6,8 +6,8 @@ import (
 )
 
 type WorkflowServerConfig struct {
-	ArgoToken   string `envconfig:"ARGO_WORKFLOWS_TOKEN" required:"true"`
-	ArgoAddress string `envconfig:"ARGO_WORKFLOWS_ADDRESS" required:"true"`
+	ArgoToken   string `envconfig:"ARGO_WORKFLOWS_TOKEN" required:"false"`
+	ArgoAddress string `envconfig:"ARGO_WORKFLOWS_ADDRESS" required:"false"`
 	CreateCRD   bool   `envconfig:"ARGO_WORKFLOWS_CREATE_CRD" default:"true"`
 	Namespace   string `envconfig:"ARGO_WORKFLOWS_NAMESPACE" default:"default"`
 	KubeConfig  string `envconfig:"KUBE_CONFIG" default:""`
