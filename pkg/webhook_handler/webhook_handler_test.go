ebfaddf86a2f4a2e2eb0860f6b6a879f406bb8e6
Gosha <gosha@rookout.com>
2023-06-29 09:43:37 +0300

    fix: event selection more specific
    Signed-off-by: Gosha <gosha@rookout.com>

--- pkg/webhook_handler/webhook_handler_test.go
+++ pkg/webhook_handler/webhook_handler_test.go
@@ -106,7 +106,7 @@ func TestPrepareBatchForMatchingTriggers(t *testing.T) {
 		payload               *git_provider.WebhookPayload
 		expectedWorkflowBatch []*common.WorkflowsBatch
 	}{
-		{name: "test1",
+		{name: "Event without action",
 			triggers: &[]Trigger{{
 				Events:    &[]string{"event1", "event2.action2"},
 				Branches:  &[]string{"branch1", "branch2"},
@@ -156,6 +156,283 @@ func TestPrepareBatchForMatchingTriggers(t *testing.T) {
 				},
 			},
 		},
+		{name: "Event and action",
+			triggers: &[]Trigger{{
+				Events:    &[]string{"event1", "event2.action2"},
+				Branches:  &[]string{"branch1", "branch2"},
+				Templates: &[]string{""},
+				OnStart:   &[]string{"main.yaml"},
+				OnExit:    &[]string{"exit.yaml"},
+				Config:    "default",
+			}},
+			payload: &git_provider.WebhookPayload{
+				Event:            "event2",
+				Action:           "action2",
+				Repo:             "repo1",
+				Branch:           "branch1",
+				Commit:           "commitHSA",
+				User:             "piper",
+				UserEmail:        "piper@rookout.com",
+				PullRequestURL:   "",
+				PullRequestTitle: "",
+				DestBranch:       "",
+			},
+			expectedWorkflowBatch: []*common.WorkflowsBatch{
+				&common.WorkflowsBatch{
+					OnStart: []*git_provider.CommitFile{
+						{
+							Path:    utils.SPtr(".workflows/main.yaml"),
+							Content: GetContent(".workflows/main.yaml"),
+						},
+					},
+					OnExit: []*git_provider.CommitFile{
+						{
+							Path:    utils.SPtr(".workflows/exit.yaml"),
+							Content: GetContent(".workflows/exit.yaml"),
+						},
+					},
+					Templates: []*git_provider.CommitFile{
+						&git_provider.CommitFile{
+							Path:    nil,
+							Content: nil,
+						},
+					},
+					Parameters: &git_provider.CommitFile{
+						Path:    nil,
+						Content: nil,
+					},
+					Config:  utils.SPtr("default"),
+					Payload: &git_provider.WebhookPayload{},
+				},
+			},
+		},
+		{name: "none existing branch",
+			triggers: &[]Trigger{{
+				Events:    &[]string{"event1", "event2.action2"},
+				Branches:  &[]string{"branch1", "branch2"},
+				Templates: &[]string{""},
+				OnStart:   &[]string{"main.yaml"},
+				OnExit:    &[]string{"exit.yaml"},
+				Config:    "default",
+			}},
+			payload: &git_provider.WebhookPayload{
+				Event:            "event1",
+				Action:           "branch2",
+				Repo:             "repo1",
+				Branch:           "branch1",
+				Commit:           "commitHSA",
+				User:             "piper",
+				UserEmail:        "piper@rookout.com",
+				PullRequestURL:   "",
+				PullRequestTitle: "",
+				DestBranch:       "",
+			},
+			expectedWorkflowBatch: nil,
+		},
+		{name: "none existing repo",
+			triggers: &[]Trigger{{
+				Events:    &[]string{"event1", "event2.action2"},
+				Branches:  &[]string{"branch1", "branch2"},
+				Templates: &[]string{""},
+				OnStart:   &[]string{"main.yaml"},
+				OnExit:    &[]string{"exit.yaml"},
+				Config:    "default",
+			}},
+			payload: &git_provider.WebhookPayload{
+				Event:            "event1",
+				Action:           "branch1",
+				Repo:             "non-existing",
+				Branch:           "branch1",
+				Commit:           "commitHSA",
+				User:             "piper",
+				UserEmail:        "piper@rookout.com",
+				PullRequestURL:   "",
+				PullRequestTitle: "",
+				DestBranch:       "",
+			},
+			expectedWorkflowBatch: nil,
+		},
+		{name: "missing OnStart",
+			triggers: &[]Trigger{{
+				Events:    &[]string{"event1", "event2.action2"},
+				Branches:  &[]string{"branch1", "branch2"},
+				Templates: &[]string{""},
+				OnStart:   &[]string{""},
+				OnExit:    &[]string{"exit.yaml"},
+				Config:    "default",
+			}},
+			payload: &git_provider.WebhookPayload{
+				Event:            "event1",
+				Action:           "branch1",
+				Repo:             "repo1",
+				Branch:           "branch1",
+				Commit:           "commitHSA",
+				User:             "piper",
+				UserEmail:        "piper@rookout.com",
+				PullRequestURL:   "",
+				PullRequestTitle: "",
+				DestBranch:       "",
+			},
+			expectedWorkflowBatch: nil,
+		},
+		{name: "missing OnExit",
+			triggers: &[]Trigger{{
+				Events:    &[]string{"event1", "event2.action2"},
+				Branches:  &[]string{"branch1", "branch2"},
+				Templates: &[]string{""},
+				OnStart:   &[]string{"main.yaml"},
+				OnExit:    &[]string{""},
+				Config:    "default",
+			}},
+			payload: &git_provider.WebhookPayload{
+				Event:            "event1",
+				Action:           "",
+				Repo:             "repo1",
+				Branch:           "branch1",
+				Commit:           "commitHSA",
+				User:             "piper",
+				UserEmail:        "piper@rookout.com",
+				PullRequestURL:   "",
+				PullRequestTitle: "",
+				DestBranch:       "",
+			},
+			expectedWorkflowBatch: []*common.WorkflowsBatch{
+				&common.WorkflowsBatch{
+					OnStart: []*git_provider.CommitFile{
+						{
+							Path:    utils.SPtr(".workflows/main.yaml"),
+							Content: GetContent(".workflows/main.yaml"),
+						},
+					},
+					OnExit: []*git_provider.CommitFile{
+						&git_provider.CommitFile{
+							Path:    nil,
+							Content: nil,
+						},
+					},
+					Templates: []*git_provider.CommitFile{
+						&git_provider.CommitFile{
+							Path:    nil,
+							Content: nil,
+						},
+					},
+					Parameters: &git_provider.CommitFile{
+						Path:    nil,
+						Content: nil,
+					},
+					Config:  utils.SPtr("default"),
+					Payload: &git_provider.WebhookPayload{},
+				},
+			},
+		},
+		{name: "Multiple OnStart",
+			triggers: &[]Trigger{{
+				Events:    &[]string{"event1", "event2.action2"},
+				Branches:  &[]string{"branch1", "branch2"},
+				Templates: &[]string{""},
+				OnStart:   &[]string{"main.yaml", "main.yaml"},
+				OnExit:    &[]string{""},
+				Config:    "default",
+			}},
+			payload: &git_provider.WebhookPayload{
+				Event:            "event1",
+				Action:           "",
+				Repo:             "repo1",
+				Branch:           "branch1",
+				Commit:           "commitHSA",
+				User:             "piper",
+				UserEmail:        "piper@rookout.com",
+				PullRequestURL:   "",
+				PullRequestTitle: "",
+				DestBranch:       "",
+			},
+			expectedWorkflowBatch: []*common.WorkflowsBatch{
+				&common.WorkflowsBatch{
+					OnStart: []*git_provider.CommitFile{
+						{
+							Path:    utils.SPtr(".workflows/main.yaml"),
+							Content: GetContent(".workflows/main.yaml"),
+						},
+						{
+							Path:    utils.SPtr(".workflows/main.yaml"),
+							Content: GetContent(".workflows/main.yaml"),
+						},
+					},
+					OnExit: []*git_provider.CommitFile{
+						&git_provider.CommitFile{
+							Path:    nil,
+							Content: nil,
+						},
+					},
+					Templates: []*git_provider.CommitFile{
+						&git_provider.CommitFile{
+							Path:    nil,
+							Content: nil,
+						},
+					},
+					Parameters: &git_provider.CommitFile{
+						Path:    nil,
+						Content: nil,
+					},
+					Config:  utils.SPtr("default"),
+					Payload: &git_provider.WebhookPayload{},
+				},
+			},
+		},
+		{name: "Multiple OnExit",
+			triggers: &[]Trigger{{
+				Events:    &[]string{"event1", "event2.action2"},
+				Branches:  &[]string{"branch1", "branch2"},
+				Templates: &[]string{""},
+				OnStart:   &[]string{"main.yaml"},
+				OnExit:    &[]string{"exit.yaml", "exit.yaml"},
+				Config:    "default",
+			}},
+			payload: &git_provider.WebhookPayload{
+				Event:            "event1",
+				Action:           "",
+				Repo:             "repo1",
+				Branch:           "branch1",
+				Commit:           "commitHSA",
+				User:             "piper",
+				UserEmail:        "piper@rookout.com",
+				PullRequestURL:   "",
+				PullRequestTitle: "",
+				DestBranch:       "",
+			},
+			expectedWorkflowBatch: []*common.WorkflowsBatch{
+				&common.WorkflowsBatch{
+					OnStart: []*git_provider.CommitFile{
+						{
+							Path:    utils.SPtr(".workflows/main.yaml"),
+							Content: GetContent(".workflows/main.yaml"),
+						},
+					},
+					OnExit: []*git_provider.CommitFile{
+						&git_provider.CommitFile{
+							Path:    utils.SPtr(".workflows/exit.yaml"),
+							Content: GetContent(".workflows/exit.yaml"),
+						},
+						&git_provider.CommitFile{
+							Path:    utils.SPtr(".workflows/exit.yaml"),
+							Content: GetContent(".workflows/exit.yaml"),
+						},
+					},
+					Templates: []*git_provider.CommitFile{
+						&git_provider.CommitFile{
+							Path:    nil,
+							Content: nil,
+						},
+					},
+					Parameters: &git_provider.CommitFile{
+						Path:    nil,
+						Content: nil,
+					},
+					Config:  utils.SPtr("default"),
+					Payload: &git_provider.WebhookPayload{},
+				},
+			},
+		},
 	}
 
 	for _, test := range tests {
@@ -168,37 +445,48 @@ func TestPrepareBatchForMatchingTriggers(t *testing.T) {
 				},
 			}
 			WorkflowsBatches, err := wh.PrepareBatchForMatchingTriggers(&ctx)
-			assert.Nil(err)
-			for iwf, wf := range WorkflowsBatches {
-				for i, _ := range wf.OnStart {
-					assert.Equal(*WorkflowsBatches[iwf].OnStart[i].Path, *test.expectedWorkflowBatch[iwf].OnStart[i].Path)
-					assert.Equal(*WorkflowsBatches[iwf].OnStart[i].Content, *test.expectedWorkflowBatch[iwf].OnStart[i].Content)
-				}
-				for j, _ := range wf.OnExit {
-					assert.Equal(*WorkflowsBatches[iwf].OnExit[j].Path, *test.expectedWorkflowBatch[iwf].OnExit[j].Path)
-					assert.Equal(*WorkflowsBatches[iwf].OnExit[j].Content, *test.expectedWorkflowBatch[iwf].OnExit[j].Content)
-				}
-
-				for k, _ := range wf.Templates {
-					if test.expectedWorkflowBatch[iwf].Templates[k].Path == nil || test.expectedWorkflowBatch[iwf].Templates[k].Content == nil {
-						assert.Nil(WorkflowsBatches[iwf].Templates[k].Path)
-						assert.Nil(WorkflowsBatches[iwf].Templates[k].Content)
-					} else {
-						assert.Equal(*WorkflowsBatches[iwf].Templates[k].Path, *test.expectedWorkflowBatch[iwf].Templates[k].Path)
-						assert.Equal(*WorkflowsBatches[iwf].Templates[k].Content, *test.expectedWorkflowBatch[iwf].Templates[k].Content)
+			if test.expectedWorkflowBatch == nil {
+				assert.Nil(WorkflowsBatches)
+				assert.NotNil(err)
+			} else {
+				assert.Nil(err)
+				for iwf, wf := range WorkflowsBatches {
+					for i, _ := range wf.OnStart {
+						assert.Equal(*test.expectedWorkflowBatch[iwf].OnStart[i].Path, *WorkflowsBatches[iwf].OnStart[i].Path)
+						assert.Equal(*test.expectedWorkflowBatch[iwf].OnStart[i].Content, *WorkflowsBatches[iwf].OnStart[i].Content)
+					}
+					for j, _ := range wf.OnExit {
+						if test.expectedWorkflowBatch[iwf].OnExit[j].Path == nil || test.expectedWorkflowBatch[iwf].OnExit[j].Content == nil {
+							assert.Nil(WorkflowsBatches[iwf].Templates[j].Path)
+							assert.Nil(WorkflowsBatches[iwf].Templates[j].Content)
+						} else {
+							assert.Equal(*test.expectedWorkflowBatch[iwf].OnExit[j].Path, *WorkflowsBatches[iwf].OnExit[j].Path)
+							assert.Equal(*test.expectedWorkflowBatch[iwf].OnExit[j].Content, *WorkflowsBatches[iwf].OnExit[j].Content)
+						}
 					}
 
-				}
+					for k, _ := range wf.Templates {
+						if test.expectedWorkflowBatch[iwf].Templates[k].Path == nil || test.expectedWorkflowBatch[iwf].Templates[k].Content == nil {
+							assert.Nil(WorkflowsBatches[iwf].Templates[k].Path)
+							assert.Nil(WorkflowsBatches[iwf].Templates[k].Content)
+						} else {
+							assert.Equal(*test.expectedWorkflowBatch[iwf].Templates[k].Path, *WorkflowsBatches[iwf].Templates[k].Path)
+							assert.Equal(*test.expectedWorkflowBatch[iwf].Templates[k].Content, *WorkflowsBatches[iwf].Templates[k].Content)
+						}
 
-				if test.expectedWorkflowBatch[iwf].Parameters.Path == nil || test.expectedWorkflowBatch[iwf].Parameters.Content == nil {
-					assert.Nil(WorkflowsBatches[iwf].Parameters.Path)
-					assert.Nil(WorkflowsBatches[iwf].Parameters.Content)
-				} else {
-					assert.Equal(*WorkflowsBatches[iwf].Parameters.Path, *test.expectedWorkflowBatch[iwf].Parameters.Path)
-				}
-				assert.Equal(*WorkflowsBatches[iwf].Config, *test.expectedWorkflowBatch[iwf].Config)
+					}
 
+					if test.expectedWorkflowBatch[iwf].Parameters.Path == nil || test.expectedWorkflowBatch[iwf].Parameters.Content == nil {
+						assert.Nil(WorkflowsBatches[iwf].Parameters.Path)
+						assert.Nil(WorkflowsBatches[iwf].Parameters.Content)
+					} else {
+						assert.Equal(*test.expectedWorkflowBatch[iwf].Parameters.Path, *WorkflowsBatches[iwf].Parameters.Path)
+					}
+					assert.Equal(*test.expectedWorkflowBatch[iwf].Config, *WorkflowsBatches[iwf].Config)
+
+				}
 			}
+
 		})
 	}
 
