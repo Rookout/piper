31f4b331d8b6e4443371502481e7999c1e451868
Gosha <gosha@rookout.com>
2023-06-29 21:53:58 +0300

    extend nginx timeout
    Signed-off-by: Gosha <gosha@rookout.com>

--- scripts/init-nginx.sh
+++ scripts/init-nginx.sh
@@ -7,6 +7,7 @@ if [ -z "$(kubectl get pods --all-namespaces | grep ingress-nginx-controller)" ]
   kubectl wait --namespace ingress-nginx \
          --for=condition=complete job/ingress-nginx-admission-create \
          --timeout=180s && \
+  kubectl rollout restart deployment ingress-nginx-controller --namespace ingress-nginx && \
   kubectl wait --namespace ingress-nginx \
          --for=condition=ready pod \
          --selector=app.kubernetes.io/component=controller \
