924a15808ffb8dd14eefc92c78ee80028b2acb4e
Adi Ludmer <Adi.Ludmer@qedma.com>
2023-07-01 12:40:25 +0300

    formalize supported k8s subscriber event types
    Signed-off-by: Adi Ludmer <Adi.Ludmer@qedma.com>

--- pkg/listener/k8s_resource_events_subscriber.go
+++ pkg/listener/k8s_resource_events_subscriber.go
@@ -1,8 +1,10 @@
 package listener
 
 import (
+	"errors"
 	"fmt"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/strings/slices"
 	"time"
 
 	"k8s.io/client-go/kubernetes"
@@ -18,6 +20,14 @@ type K8sResourceEventsSubscriber struct {
 	stopCh    chan struct{}
 }
 
+const (
+	CREATED = "created"
+	UPDATED = "updated"
+	DELETED = "deleted"
+)
+
+var supportedEvents = []string{CREATED, UPDATED, DELETED}
+
 func NewK8sResourceEventsSubscriber(resource runtime.Object, namespace string) *K8sResourceEventsSubscriber {
 	return &K8sResourceEventsSubscriber{
 		resource:  resource,
@@ -29,6 +39,10 @@ func NewK8sResourceEventsSubscriber(resource runtime.Object, namespace string) *
 }
 
 func (a *K8sResourceEventsSubscriber) Subscribe(eventName string, callback func(eventData any)) error {
+	if !slices.Contains(supportedEvents, eventName) {
+		return errors.New(fmt.Sprintf("invalid event - %s, can be one of %s", eventName, supportedEvents))
+	}
+
 	err := a.pubSub.Subscribe(eventName, callback)
 	if err != nil {
 		return err
@@ -69,13 +83,13 @@ func (a *K8sResourceEventsSubscriber) start() error {
 		time.Second*0,
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				_ = a.pubSub.Publish(fmt.Sprintf("%s_created", resourceKind), obj)
+				_ = a.pubSub.Publish(CREATED, obj)
 			},
 			UpdateFunc: func(oldObj, newObj interface{}) {
-				_ = a.pubSub.Publish(fmt.Sprintf("%s_updated", resourceKind), oldObj)
+				_ = a.pubSub.Publish(UPDATED, oldObj)
 			},
 			DeleteFunc: func(obj interface{}) {
-				_ = a.pubSub.Publish(fmt.Sprintf("%s_deleted", resourceKind), obj)
+				_ = a.pubSub.Publish(DELETED, obj)
 			},
 		},
 	)
