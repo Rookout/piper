ef4ed1f4eec9800a920a1293bdcace1ae4bd7735
Gosha <gosha@rookout.com>
2023-07-09 00:23:39 +0300

    fix: graceful shutdown handler
    Signed-off-by: Gosha <gosha@rookout.com>

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -5,12 +5,14 @@ import (
 	"github.com/rookout/piper/pkg/conf"
 	"golang.org/x/net/context"
 	"log"
+	"net/http"
 	"time"
 )
 
 func Start(ctx context.Context, stop context.CancelFunc, cfg *conf.GlobalConfig, clients *clients.Clients) {
 
 	srv := NewServer(cfg, clients)
+	gracefulShutdownHandler := newGracefulShutdown(ctx, stop)
 	httpServer := srv.ListenAndServe()
 
 	err := clients.GitProvider.SetWebhook()
@@ -18,34 +20,45 @@ func Start(ctx context.Context, stop context.CancelFunc, cfg *conf.GlobalConfig,
 		panic(err)
 	}
 
+	log.Println("shutting down gracefully...")
+
+	gracefulShutdownHandler.shutdown(httpServer, clients)
+
+	log.Println("Server exiting")
+}
+
+type gracefulShutdown struct {
+	ctx  context.Context
+	stop context.CancelFunc
+}
+
+func newGracefulShutdown(ctx context.Context, stop context.CancelFunc) *gracefulShutdown {
+	return &gracefulShutdown{
+		ctx:  ctx,
+		stop: stop,
+	}
+}
+
+func (s *gracefulShutdown) shutdown(httpServer *http.Server, clients *clients.Clients) {
 	// Listen for the interrupt signal.
-	<-ctx.Done()
+	<-s.ctx.Done()
 
 	// Restore default behavior on the interrupt signal and notify user of shutdown.
-	stop()
-	log.Println("shutting down gracefully...")
+	s.stop()
 
 	// The context is used to inform the server it has 10 seconds to finish
 	// the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	_ = gracefulShutdownHandler(&ctx, clients)
+	err := clients.GitProvider.UnsetWebhook(&ctx)
+	if err != nil {
+		log.Println("Unset webhook error: ", err) // ERROR
+	}
 
 	err = httpServer.Shutdown(ctx)
 	if err != nil {
 		log.Fatal("Server forced to shutdown: ", err)
 	}
 
-	log.Println("Server exiting")
-}
-
-func gracefulShutdownHandler(ctx *context.Context, clients *clients.Clients) error {
-	err := clients.GitProvider.UnsetWebhook(ctx)
-	if err != nil {
-		log.Println("Unset webhook error: ", err) // ERROR
-		return err
-	}
-
-	return nil
 }
