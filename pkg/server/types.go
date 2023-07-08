cfee4fc10b3984a1c224b6b172ae0f2b4d3e9b6b
Gosha <gosha@rookout.com>
2023-07-08 23:09:43 +0300

    fix: improved server managment
    Signed-off-by: Gosha <gosha@rookout.com>

--- pkg/server/types.go
+++ pkg/server/types.go
@@ -2,26 +2,26 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rookout/piper/pkg/clients"
 	"github.com/rookout/piper/pkg/conf"
+	"github.com/rookout/piper/pkg/server/routes"
 	"github.com/rs/zerolog"
-	"net/http"
 )
 
 type Server struct {
-	router  *gin.Engine
-	logger  *zerolog.Logger
-	config  *conf.GlobalConfig
-	handler http.Handler
+	router *gin.Engine
+	logger *zerolog.Logger
+	config *conf.GlobalConfig
 }
 
-func NewServer(config *conf.GlobalConfig, logger *zerolog.Logger) (*Server, error) {
+func NewServer(config *conf.GlobalConfig, logger *zerolog.Logger) *Server {
 	srv := &Server{
 		router: gin.New(),
 		logger: logger,
 		config: config,
 	}
 
-	return srv, nil
+	return srv
 }
 
 func (s *Server) registerMiddlewares() {
@@ -32,4 +32,10 @@ func (s *Server) registerMiddlewares() {
 		gin.Recovery(),
 	)
 
+}
+
+func (s *Server) getRoutes(cfg *conf.GlobalConfig, clients *clients.Clients, router *gin.Engine) {
+	v1 := router.Group("/")
+	routes.AddHealthRoutes(cfg, v1)
+	routes.AddWebhookRoutes(cfg, clients, v1)
 }
